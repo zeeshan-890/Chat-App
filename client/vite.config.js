b1c724d7df1a5e6b278ae424ba2cc476b2df5e6d
zeeshan-890 <zzabbaskhan830@gmail.com>
2025-08-01 13:53:06 +0500

    fixed

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,13 +1,20 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
-     tailwindcss(),react()
-    ],
-    // define: {
-    //   global: 'window', // <-- This is the key fix!
-    // },
+    , react()
+  ],
+  // define: {
+  //   global: 'window', // <-- This is the key fix!
+  // },
+  build: {
+    outDir: 'dist',
+    sourcemap: false
+  },
+  server: {
+    port: 5173
+  }
 })
